67d9a6bd38937f3a72a7ceaeab50c8730f9e2b97
Manish Kumar Singh <0896memanish@gmail.com>
2025-02-17 15:26:58 +0530

    Added Controller for CreateProduct feature vertical slice. The controller uses Carter for exposing HTTP POST We are using MediatR for implementing CQRS design pattern.

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,6 +1,35 @@
+using Carter;
+using Mapster;
+using MediatR;
+
 namespace Catalog.API.Products.CreateProduct;
 
-public class CreateProductEndpoint
+public record CreateProductRequest(
+    string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price);
+
+public record CreateProductResponse(Guid Id);
+
+// Controller Class
+public class CreateProductEndpoint : ICarterModule //ICarterModule implemented classes are 
 {
-    
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateProductCommand>();
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<CreateProductResponse>();
+
+                return Results.Created($"/products/{result.Id}", response);
+            }).WithName("CreateProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create Product")
+            .WithDescription("Create Product");
+    }
 }
